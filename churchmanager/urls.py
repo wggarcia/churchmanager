1b3950e4cdb271e73588320221697f5e5d5919d7
wagner garcia <angelica@MacBook-Pro-de-Wagner-Garcia.local>
2025-10-24 15:10:34 -0300

    Corrigindo exibição de imagens no Render

--- churchmanager/urls.py
+++ churchmanager/urls.py
@@ -1,13 +1,20 @@
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
 ]
 
-# ⚙️ Mostra os arquivos de imagem (plano de fundo, logo, etc.)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # 🔹 Serve mídia mesmo em produção no Render
+    from django.views.static import serve
+    from django.urls import re_path
+
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
